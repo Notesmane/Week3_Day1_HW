0d9f1969e04b1eb997d453052ae80f87dd3caa22
Ronnie Powell <notesmane@mac-pro.myfiosgateway.com>
2023-07-11 14:57:43 -0400

    created person class

--- homework.js
+++ homework.js
@@ -1,6 +1,6 @@
 
 class Hamster {
-    constructor(owner, name, price) {
+    constructor(owner, name = '', price = 15) {
         this.owner = owner;
         this.name = name;
         this.price = price;
@@ -15,4 +15,60 @@ class Hamster {
         return this.price
     }
 }
+
+// const furry = new Hamster('Sam', 'Furry',)
+// console.log(furry);
+// const
+
+// ==================================================================================
+
+class Person {
+    constructor(name = 'Ronnie', age, height, weight, mood = 0, hamsters = [], bankAccount = 0){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.mood = mood;
+        this.hamsters = hamsters;
+        this.bankAccount = bankAccount;
+        this.greet = '';
+        this.eat = eat;
+        this.exercise = exercise;
+        this.ageUp = ageUp;
+    }
+    getName() {
+        return this.name;
+    }
+    getAge() {
+        return this.age;
+    }
+    getWeight() {
+        return this.weight;
+    }
+    greet() {
+        console.log();
+    }
+    eat() {
+        this.weight++;
+        this.mood++;
+    }
+    eatXTimes(num) {
+        this.weight += num
+    }
+    exercise() {
+        this.weight--;
+    }
+    ageUp() {
+        this.age++;
+        this.height++;
+        this.weight++;
+        this.mood--;
+        this.bankAccount+=10;
+    }
+    buyHamster() {
+        Hamster.push();
+        this.mood+=10;
+        this.bankAccount+=this.hamsters.getPrice();
+    }
+
+} //console.log(Person);
 
