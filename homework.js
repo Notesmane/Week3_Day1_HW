ef261b5b1fc255dfab2a1e639baea945e39f9ecc
Ronnie Powell <notesmane@mac-pro.myfiosgateway.com>
2023-07-29 22:19:12 -0400

    HW Completed

--- homework.js
+++ homework.js
@@ -16,14 +16,17 @@ class Hamster {
     }
 }
 
-// const furry = new Hamster('Sam', 'Furry',)
+const furry = new Hamster('Sam', 'Furry', 15)
 // console.log(furry);
-// const
+furry.wheelRun();
+furry.eatFood();
+const price = furry.getPrice();
+console.log(furry);
 
 // ==================================================================================
 
 class Person {
-    constructor(name = 'Ronnie', age, height, weight, mood = 0, hamsters = [], bankAccount = 0){
+    constructor(name, age = 0, height = 0, weight = 0, mood = 0, hamsters = [], bankAccount = 0){
         this.name = name;
         this.age = age;
         this.height = height;
@@ -32,9 +35,9 @@ class Person {
         this.hamsters = hamsters;
         this.bankAccount = bankAccount;
         this.greet = '';
-        this.eat = eat;
-        this.exercise = exercise;
-        this.ageUp = ageUp;
+        // this.eat = eat;
+        // this.exercise = exercise;
+        // this.ageUp = ageUp;
     }
     getName() {
         return this.name;
@@ -51,12 +54,16 @@ class Person {
     eat() {
         this.weight++;
         this.mood++;
+        console.log(this.weight);
+        console.log(this.mood);
     }
     eatXTimes(num) {
         this.weight += num
+        console.log
     }
     exercise() {
         this.weight--;
+        console.log(this.weight);
     }
     ageUp() {
         this.age++;
@@ -64,26 +71,109 @@ class Person {
         this.weight++;
         this.mood--;
         this.bankAccount+=10;
+        console.log(this.age);
+        console.log(this.height);
+        console.log(this.weight);
+        console.log(this.mood);
+        console.log(this.bankAccount);
     }
-    buyHamster() {
-        Hamster.push();
+    buyHamster(hamster) {
+        this.hamsters.push(hamster);
         this.mood+=10;
-        this.bankAccount+=this.hamsters.getPrice();
+        this.bankAccount -= hamster.getPrice();
+        console.log(this.hamsters);
+        console.log(this.mood);
+        console.log(this.bankAccount);
     }
 
 } //console.log(Person);
 
 const Timmy = new Person('Timmy', 5,);
-Timmy.eatXTimes(5);
-Timmy.exercise(5);
-Timmy.ageUp(9);
-Timmy.buyHamster(Gus);
-Timmy.ageUp(15);
-Timmy.eatXTimes(2);
-Timmy.exercise(2);
 
-const Gus = new Hamster('Timmy');
+// let num2 = Timmy.eatXTimes(5);
+// console.log(`He ate ${num2} times.`)
+
+for(let i = 1; i <= 5; i++) {
+    Timmy.eat()
+    console.log(`Ate ${i} times`)
+}
+
+for(let i = 1; i <= 5; i++) {
+    Timmy.exercise()
+    console.log(`he exercised ${i} times`)
+}
+
+Timmy.ageUp(9);
+
+// console.log(Timmy);
+// Timmy.exercise(5);
+// Timmy.ageUp(9);
+// Timmy.buyHamster(Gus);
+// Timmy.ageUp(15);
+// Timmy.eatXTimes(2);
+// Timmy.exercise(2);
+
+const Gus = new Hamster('Timmy', 'Gus');
+
+Timmy.buyHamster(Gus);
+
+Timmy.ageUp(15);
+
+for(let i = 1; i <= 2; i++) {
+    Timmy.eat()
+    console.log(`Timmy ate ${i} times`)
+}
+
+for(let i = 1; i <= 2; i++) {
+    Timmy.exercise()
+    console.log(`and he exercised ${i} times`)
+}
 
 // ===============================================================================
 
 
+class Dinner {
+    constructor(appetizer, entree, dessert) {
+        this.appetizer = appetizer,
+        this.entree = entree;
+        this.dessert = dessert;
+    }
+    getAppetizer() {
+        this.appetizer;
+    }
+    getEntree() {
+        this.entree;
+    }
+    getDessert() {
+        this.dessert;
+    }
+
+}
+
+class Chef {
+
+        preparingFood(appetizer, entree, dessert){
+            const dinner = new Dinner(appetizer, entree, dessert);
+            console.log(dinner)
+        }
+}
+
+const dinner1 = new Dinner('Mozzarella Sticks','Chicken Wings','Lemon Pie')
+console.log(dinner1);
+
+const dinner2 = new Dinner ('', 'Edamame Beans','Shrimp Pad Thai','Thai Iced Tea');
+console.log(dinner2);
+
+const dinner3 = new Dinner ('', 'Platano Maduros','Grilled Chicken with Rice and Beans','Flan');
+console.log(dinner3);
+
+// console.logchef1.preparingFood('Edamame Beans','Shrimp Pad Thai','Thai Iced Tea');
+// chef1.preparingFood('Platano Maduros','Grilled Chicken with Rice and Beans','Flan');
+
+// const dinner1 = new Dinner ('', ');
+// const dinner2 = new Dinner ('', 'Edamame Beans','Shrimp Pad Thai','Thai Iced Tea');
+// const dinner3 = new Dinner ('', 'Platano Maduros','Grilled Chicken with Rice and Beans','Flan');
+
+// console.log(`The first dinner will be ${dinner1}`);
+// console.log(`The next dinner will be ${dinner2}`);
+// console.log(`And the last dinner will be ${dinner3}`);
